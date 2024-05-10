3c679f72584ef304c253d3e36a71675949178ff0
NN <95127494+KaningNoppasin@users.noreply.github.com>
2024-05-10 21:48:12 +0700

    day 5 Database Programming with Prisma ORM Advanced

--- day4 Database Programming/server.js
+++ day4 Database Programming/server.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 
 const { PrismaClient } = require('@prisma/client');
+const { error } = require('console');
 const prisma = new PrismaClient();
 
 app.use(bodyParser.json());
@@ -45,5 +46,210 @@ app.get('/book/list', async (req, res) => {
     const data = await prisma.book.findMany();
     res.send({data: data});
 })
+
+// day 5 Database Programming with Prisma ORM Advanced
+
+app.post('/book/create', async (req, res) => {
+    const data = req.body;
+    const result = await prisma.book.create({
+        data: data
+    });
+    // INSERT INTO Book(isbn, name, price) VALUES(:isbn, :name, :price)
+
+    res.send({result: result});
+});
+
+app.post('/book/createManual', async (req, res) => {
+    const result = await prisma.book.create({
+        data: {
+            isbn: '1004',
+            name: 'Flutter',
+            price: 850,
+        }
+    });
+    res.send({result: result});
+});
+
+app.put('/book/update/:id', async (req, res) => {
+    try {
+        await prisma.book.update({
+            data: {
+                isbn: '10022',
+                name: 'test update',
+                price: 900,
+            },
+            where: {
+                id: parseInt(req.params.id)
+            }
+        });
+
+        res.send({ message: 'success'});
+    } catch (e) {
+        res.status(500).send({error: e.message});
+    }
+});
+
+app.delete('/book/remove/:id', async (req, res) => {
+    try {
+        await prisma.book.delete({
+            where: {
+                id: parseInt(req.params.id)
+            }
+        });
+        res.send({message: 'success'});
+    } catch (e) {
+        res.status(500).send({error: e.message});
+    }
+});
+
+
+app.post('/book/search', async (req, res) => {
+    try {
+        const keyword = req.body.keyword;
+        const data = await prisma.book.findMany({
+            where: {
+                name: {
+                    contains: keyword
+                }
+            }
+        });
+
+        res.send({results: data});
+    } catch (e) {
+        res.status(500).send({error: e.message});
+    }
+});
+
+app.post('/book/startsWith', async (req, res) => {
+    try {
+        const keyword = req.body.keyword;
+        const data = await prisma.book.findMany({
+            where: {
+                name: {
+                    startsWith: keyword
+                }
+            }
+        });
+        res.send({results: data});
+    } catch (e) {
+        res.status(500).send({error: e.message});
+    }
+});
+
+app.post('/book/endsWith', async (req, res) => {
+    try {
+        const keyword = req.body.keyword;
+        const data = await prisma.book.findMany({
+            where: {
+                name: {
+                    endsWith: keyword
+                }
+            }
+        });
+        res.send({results: data});
+    } catch (e) {
+        res.status(500).send({error: e.message});
+    }
+});
+
+app.get('/book/orderBy', async (req, res) => {
+    try {
+        const data = await prisma.book.findMany({
+            orderBy: {
+                price: 'desc'
+            }
+        });
+        res.send({results: data});
+    } catch (e) {
+        res.status(500).send({error: e.message});
+    }
+});
+
+app.get('/book/gt', async (req, res) => {
+    try {
+        const data = await prisma.book.findMany({
+            where: {
+                price:{
+                    gt: 900 // > 900, >= 900 gte
+                }
+            }
+        });
+        res.send({results: data});
+    } catch (e) {
+        res.status(500).send({error: e.message});
+    }
+});
+
+app.get('/book/lt', async (req, res) => {
+    try {
+        const data = await prisma.book.findMany({
+            where: {
+                price:{
+                    lt: 900 // < 900, >= 900 lte
+                }
+            }
+        });
+        res.send({results: data});
+    } catch (e) {
+        res.status(500).send({error: e.message});
+    }
+});
+
+app.get('/book/notNull', async (req, res) => {
+    try {
+        const data = await prisma.book.findMany({
+            where:{
+                detail: {
+                    not: null
+                }
+            }
+        });
+        res.send({results: data});
+    } catch (e) {
+        res.status(500).send({error: e.message});
+    }
+});
+
+// Day 6 Prisma ORM : Additional and Authentication
+
+app.get('/book/isNull', async (req, res) => {
+    try {
+        const data = await prisma.book.findMany({
+            where: {
+                detail: null
+            }
+        })
+
+        res.send({results: data});
+    } catch (e) {
+        res.status(500).send({error: e.message});
+    }
+});
+
+
+app.get('/book/between', async (req, res) => {
+    try {
+        const data = await prisma.book.findMany({
+            where: {
+                // lte first >> gte seconde
+                // AND: {
+                //     price: {
+                //         lte: 1500 // <= 1500
+                //     }},
+                //     price: {
+                //         gte: 900 //>= 900
+                //     },
+                price: {
+                    lte: 1500, // <= 1500
+                    gte: 900, // >= 900
+                }},
+        });
+
+        res.send({ results: data});
+    } catch (e) {
+        res.status(500).send({error: e.message});
+    }
+})
+
 
 app.listen(3001);
