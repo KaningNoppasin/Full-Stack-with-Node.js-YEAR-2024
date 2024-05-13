7e240688197134e7403aeda7ba2dee8275a4d10b
NN <95127494+KaningNoppasin@users.noreply.github.com>
2024-05-13 17:18:27 +0700

    add Day8 ORM Relations, PDF, Excel, Middleware, CORS

--- day4 Database Programming/server.js
+++ day4 Database Programming/server.js
@@ -9,8 +9,15 @@ const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
 dotenv.config();
 
+const fileUpload = require('express-fileupload')
+app.use(fileUpload())
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use('/upload', express.static('uploads'));
+
+const cors = require('cors');
+app.use(cors());
 
 // Day7 : ORM Relations, PDF, Excel, Middleware, CORS
 
@@ -458,6 +465,123 @@ app.get('/multiModel', async (req, res) => {
         });
 
         res.send({results: data});
+    } catch (e) {
+        res.status(500).send({error: e.message});
+    }
+});
+
+// Day8 ORM Relations, PDF, Excel, Middleware, CORS
+
+
+app.post('/book/testUpload', (req, res) =>{
+    try {
+        const myFile = req.files.myFile;
+
+        myFile.mv('./uploads/' + myFile.name, (err) => {
+            if (err) {
+                res.status(500).send({error: err});
+            }
+            res.send({message: "success"});
+        })
+    } catch (e) {
+        res.status.send({error: e.message});
+    }
+});
+
+app.get('/readFile', (req, res) => {
+    try {
+        const fs = require('fs');
+        fs.readFile('test.txt', (err, data) => {
+            if (err){
+                throw err;
+            }
+            res.send(data);
+        });
+    } catch (e) {
+        res.status(500).send({error: e.message});
+    }
+});
+
+app.get('/writeFile', (req, res) => {
+    try {
+        const fs = require('fs');
+        fs.writeFile('text.txt', 'hello by kob', (err) => {
+            if (err) {
+                throw err;
+            }
+        });
+
+        res.send({message: "success"});
+    } catch (e) {
+        res.status(500).send({error: e.message});
+    }
+});
+
+app.get('/removeFile', (req, res) => {
+    try {
+        const fs = require('fs');
+        fs.unlinkSync('test.txt');
+
+        res.send({message: "success"});
+    } catch (e) {
+        res.status(500).send({error: e.message});
+    }
+});
+
+app.get('/fileExists', (req, res) => {
+    try {
+        const fs = require('fs');
+        const found = fs.existsSync('package.json');
+
+        res.send({found: found});
+    } catch (e) {
+        res.status(500).send({error: e.message});
+    }
+});
+
+app.get('/createPdf', (req, res) => {
+    const PDFDocument = require('pdfkit');
+    const fs = require('fs')
+    const doc = new PDFDocument();
+
+    doc.pipe(fs.createWriteStream('output.pdf'));
+
+    doc
+        .fontSize(25)
+        .text('Some text adn em', 100, 100);
+
+    doc
+        .addPage()
+        .fontSize(25)
+        .text('Here is some', 100, 100);
+    doc.end();
+
+    res.send({message: "success"});
+})
+
+app.get('/readExcel', async (req, res) => {
+    try {
+        const excel = require('exceljs');
+        const wb = new excel.Workbook();
+        await wb.xlsx.readFile('productExport.xlsx');
+        const ws = wb.getWorksheet(1);
+
+        for (let i = 1; i < ws.rowCount; i++){
+            const row = ws.getRow(i);
+            const barcode = row.getCell(1).value;
+            const name = row.getCell(2).value;
+            const cost = row.getCell(3).value;
+            const sale = row.getCell(4).value;
+            const send = row.getCell(5).value;
+            const unit = row.getCell(6).value;
+            const point = row.getCell(7).value;
+            const productTypeId = row.getCell(8).value;
+
+            console.log(barcode, name, cost, sale, send, unit, point, productTypeId);
+        }
+
+        res.send({message: "success"});
+
     } catch (e) {
         res.status(500).send({error: e.message});
     }
